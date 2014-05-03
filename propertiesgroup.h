a6035b80d01e88a597ce948f1ff9f561e343a489
pl <panuins@gmail.com>
2014-05-03 16:13:39 +0800

    On branch master 	modified:   QtPropertyCodeGenerator.pro 	modified:   classsettings.cpp 	modified:   classsettings.h 	modified:   dialogedit.cpp 	modified:   dialogedit.h 	modified:   dialogset.cpp 	modified:   dialogset.h 	modified:   enumtype.cpp 	modified:   enumtype.h 	modified:   main.cpp 	modified:   propertiesgroup.cpp 	modified:   propertiesgroup.h 	modified:   propertiesgroupInlineFunctions.h 	modified:   propertiesgroupdata.cpp 	modified:   propertiesgroupdata.h 	modified:   property.cpp 	modified:   property.h 	modified:   propertyInlineFunctions.h 	modified:   propertydata.cpp 	modified:   propertydata.h 	modified:   sharedFunctions.h 	modified:   widget.cpp 	modified:   widget.h 	添加协议

--- propertiesgroup.h
+++ propertiesgroup.h
@@ -1,3 +1,17 @@
+/*****************************************************************************
+ * propertiesgroup.h:
+ *****************************************************************************
+ * Authors: panuins <panuins@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GPLv2 or later as published by the Free Software
+ * Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *****************************************************************************/
 #ifndef PROPERTIESGROUP_H
 #define PROPERTIESGROUP_H
 #include "propertiesgroupdata.h"
--- propertyInlineFunctions.h
+++ propertyInlineFunctions.h
@@ -1,3 +1,17 @@
+/*****************************************************************************
+ * propertyInlineFunctions.h:
+ *****************************************************************************
+ * Authors: panuins <panuins@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GPLv2 or later as published by the Free Software
+ * Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *****************************************************************************/
 #ifndef PROPERTYINLINEFUNCTIONS_H
 #define PROPERTYINLINEFUNCTIONS_H
 #ifdef DEBUG_PROPERTYS_COW_DETAIL
